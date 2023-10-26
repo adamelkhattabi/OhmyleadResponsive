e292cd970475c13c9abcc84eaf0d3432d0b754a0
Adam EL KHATTABI <adamelkt96@gmail.com>
2023-10-26 16:07:34 +0100

    10/26

--- script.js
+++ script.js
@@ -48,44 +48,80 @@ $(document).ready(function () {
     responsive: {
       0: {
         items: 1, // Display 1 item on small screens (less than 600px wide)
+        navText: [
+          "<div class='custom-prev'><img src='images/arrow-left-sm.svg' alt='Previous'></div>",
+          "<div class='custom-next'><img src='images/arrow-right-sm.svg' alt='Next'></div>",
+        ],
       },
       768: {
         items: 2, // Display 2 items on medium-sized screens (768px wide and above)
+        navText: [
+          "<div class='custom-prev'><img src='images/arrow-left-sm.svg' alt='Previous'></div>",
+          "<div class='custom-next'><img src='images/arrow-right-sm.svg' alt='Next'></div>",
+        ],
       },
       992: {
         items: 3, // Display 3 items on large screens (992px wide and above)
+        navText: [
+          "<div class='custom-prev'><img src='images/arrow-left-sm.svg' alt='Previous'></div>",
+          "<div class='custom-next'><img src='images/arrow-right-sm.svg' alt='Next'></div>",
+        ],
       },
       1200: {
-        items: 5, 
+        items: 5,
+        navText: [
+          "<div class='custom-prev'><img src='images/arrow-left-sm.svg' alt='Previous'></div>",
+          "<div class='custom-next'><img src='images/arrow-right-sm.svg' alt='Next'></div>",
+        ],
       },
     },
   });
 
- $(".second-carousel").owlCarousel({
-   items: 6,
-   loop: true,
-   nav: true,
-   dots: false,
-   margin: 20,
-   navText: [
-     "<button class='custom-prev'><img src='images/arrow-left.svg' alt='Previous'></button>",
-     "<button class='custom-next'><img src='images/arrow-right.svg' alt='Next'></button>",
-   ],
-   responsive: {
-     0: {
-       items: 2,
-       margin: 10, // Adjust the margin for smaller screens
-     },
-     768: {
-       items: 3,
-       margin: 15, // Adjust the margin for medium-sized screens
-     },
-     992: {
-       items: 4,
-       margin: 20, // Adjust the margin for larger screens
-     },
-   },
- });
+  $(".second-carousel").owlCarousel({
+    items: 6,
+    loop: true,
+    nav: true,
+    dots: false,
+    margin: 20,
+    navText: [
+      "<button class='custom-prev'><img src='images/arrow-left.svg' alt='Previous'></button>",
+      "<button class='custom-next'><img src='images/arrow-right.svg' alt='Next'></button>",
+    ],
+    responsive: {
+      0: {
+        items: 2,
+        margin: 10, // Adjust the margin for smaller screens
+        navText: [
+          "<div class='custom-prev'><img src='images/arrow-left-sm.svg' alt='Previous'></div>",
+          "<div class='custom-next'><img src='images/arrow-right-sm.svg' alt='Next'></div>",
+        ],
+      },
+      768: {
+        items: 3,
+        margin: 15, // Adjust the margin for medium-sized screens
+        navText: [
+          "<div class='custom-prev'><img src='images/arrow-left-sm.svg' alt='Previous'></div>",
+          "<div class='custom-next'><img src='images/arrow-right-sm.svg' alt='Next'></div>",
+        ],
+      },
+      992: {
+        items: 3,
+        margin: 20, // Adjust the margin for larger screens
+        navText: [
+          "<button class='custom-prev'><img src='images/arrow-left.svg' alt='Previous'></button>",
+          "<button class='custom-next'><img src='images/arrow-right.svg' alt='Next'></button>",
+        ],
+      },
+      1220: {
+        items: 3,
+        margin: 20, // Adjust the margin for larger screens
+        navText: [
+          "<button class='custom-prev'><img src='images/arrow-left.svg' alt='Previous'></button>",
+          "<button class='custom-next'><img src='images/arrow-right.svg' alt='Next'></button>",
+        ],
+      },
+    },
+  });
 
   // Target the last carousel and enable dots and navigation
   $(".last-carousel").owlCarousel({
@@ -100,12 +136,21 @@ $(document).ready(function () {
     ],
     responsive: {
       0: {
-        items: 1, // Display 1 item on small screens (less than 600px wide)
+        items: 1.2, // Display 1 item on small screens (less than 600px wide)
       },
-      768: {
+      400: {
+        items: 1.2, // Display 1 item on small screens (less than 600px wide)
+      },
+      600: {
+        items: 1.6, // Display 2 items on medium-sized screens (768px wide and above)
+      },
+      800: {
         items: 2, // Display 2 items on medium-sized screens (768px wide and above)
       },
+
       992: {
+        margin: 0,
+
         items: 3, // Display 3 items on large screens (992px wide and above)
       },
     },
